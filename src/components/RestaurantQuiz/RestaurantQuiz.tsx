ca7e897c4dc426d0f974cd5ca9ccac5b656aedd7
ggr <ginagonzalezroundey@gmail.com>
2022-11-07 13:37:37 -0800

    swap widget id style

--- src/components/RestaurantQuiz/RestaurantQuiz.tsx
+++ src/components/RestaurantQuiz/RestaurantQuiz.tsx
@@ -43,81 +43,79 @@ const RestaurantQuiz: React.FC = () => {
   }, [features, setFeatures]);
 
   return (
-    <div id="turkish-begins-body">
-      <div className="restaurant-quiz-container container">
-        <h1>Restaurants Quiz</h1>
-        <div className="card filter-card">
-          <h2>Filters</h2>
-          <div className="row justify-content-center">
-            <div className="col-auto">
-              <label>Min Budget</label>
-              <input
-                className="form-control"
-                type="number"
-                value={minBudget}
-                onChange={(val): void => { setMinBudget(+val.target.value); }}
-              />
-            </div>
-            <div className="col-auto">
-              <label>Max Budget</label>
-              <input
-                className="form-control"
-                type="number"
-                value={maxBudget}
-                onChange={(val): void => { setMaxBudget(+val.target.value); }}
-              />
-            </div>
-            <div className="col-auto">
-              <label>Cuisine</label>
-              <select
-                className="form-select"
-                value={cuisine}
-                onChange={(val): void => { setCuisine(val.target.value as Cuisine); }}
-              >
-                <option value={undefined}>Anything</option>
-                <option value={Cuisine.COUNTRY}>Turkish</option>
-                <option value={Cuisine.NOT_COUNTRY}>Non-Turkish</option>
-              </select>
-            </div>
+    <div className="restaurant-quiz-container container">
+      <h1>Restaurants Quiz</h1>
+      <div className="card filter-card">
+        <h2>Filters</h2>
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <label>Min Budget</label>
+            <input
+              className="form-control"
+              type="number"
+              value={minBudget}
+              onChange={(val): void => { setMinBudget(+val.target.value); }}
+            />
           </div>
-          <div className="row justify-content-center feature-list">
-            {(createFeatureIterable()).map((feature) => (
-              <div className="col-auto" key={feature}>
-                <div className="form-check form-check-inline">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id={feature}
-                    checked={!!features[feature]}
-                    onChange={(val): void => {
-                      toggleFeature(val.target.checked, feature);
-                    }}
-                  />
-                  <label className="form-check-label" htmlFor={feature}>
-                    {formatScreamSnakeCase(feature)}
-                  </label>
-                </div>
+          <div className="col-auto">
+            <label>Max Budget</label>
+            <input
+              className="form-control"
+              type="number"
+              value={maxBudget}
+              onChange={(val): void => { setMaxBudget(+val.target.value); }}
+            />
+          </div>
+          <div className="col-auto">
+            <label>Cuisine</label>
+            <select
+              className="form-select"
+              value={cuisine}
+              onChange={(val): void => { setCuisine(val.target.value as Cuisine); }}
+            >
+              <option value={undefined}>Anything</option>
+              <option value={Cuisine.COUNTRY}>Turkish</option>
+              <option value={Cuisine.NOT_COUNTRY}>Non-Turkish</option>
+            </select>
+          </div>
+        </div>
+        <div className="row justify-content-center feature-list">
+          {(createFeatureIterable()).map((feature) => (
+            <div className="col-auto" key={feature}>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={feature}
+                  checked={!!features[feature]}
+                  onChange={(val): void => {
+                    toggleFeature(val.target.checked, feature);
+                  }}
+                />
+                <label className="form-check-label" htmlFor={feature}>
+                  {formatScreamSnakeCase(feature)}
+                </label>
               </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="text-center">
+        {loading && <div>Loading...</div>}
+        {error && <div className="error">Error: {error.message} </div>}
+        {(!loading && !!resultList) && (
+          <div>
+            {resultList.length === 0 && <div>No restaurant data to show</div>}
+            {resultList.map((restaurant) => (
+              <RestaurantItem
+                key={restaurant.id}
+                restaurant={restaurant}
+                selectedFeatures={features}
+                fromQuiz={true}
+              />
             ))}
           </div>
-        </div>
-        <div className="text-center">
-          {loading && <div>Loading...</div>}
-          {error && <div className="error">Error: {error.message} </div>}
-          {(!loading && !!resultList) && (
-            <div>
-              {resultList.length === 0 && <div>No restaurant data to show</div>}
-              {resultList.map((restaurant) => (
-                <RestaurantItem
-                  key={restaurant.id}
-                  restaurant={restaurant}
-                  selectedFeatures={features}
-                  fromQuiz={true}
-                />
-              ))}
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
   );
